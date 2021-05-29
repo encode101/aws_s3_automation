d886717a43b8f3b2ff3c00c16ccca6e0ef24f508
Rahul Mishra <rahul.mishra@koruux.com>
2021-05-29 14:48:06 +0530

    interim

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. This makes me Glad and Smile :)
+          Edit <code>src/App.js</code> and save to reloadsss. This makes me Glad and Smile :)
         </p>
         <a
           className="App-link"
