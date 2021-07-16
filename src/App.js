da758875252476df523bcf9c74b07b1b3f370749
Rahul Mishra <rahul.mishra@koruux.com>
2021-07-16 10:05:38 +0530

    interim

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          God Is Great!!
+          God Is Greatest!!
         </p>
         <a
           className="App-link"
